2500628a44de53ac3a2deeec11f2448a07c7ad5d
Prabhakar Das <shus21@gmail.com>
2025-07-06 09:59:58 +0530

    Initial Commit

---
+++ Batcore/BatcoreApp.swift
@@ -0,0 +1,17 @@
+//
+//  BatcoreApp.swift
+//  Batcore
+//
+//  Created by Prabhakar Das on 06/07/25.
+//
+
+import SwiftUI
+
+@main
+struct BatcoreApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BatcoreTests/BatcoreTests.swift
@@ -0,0 +1,17 @@
+//
+//  BatcoreTests.swift
+//  BatcoreTests
+//
+//  Created by Prabhakar Das on 06/07/25.
+//
+
+import Testing
+@testable import Batcore
+
+struct BatcoreTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
